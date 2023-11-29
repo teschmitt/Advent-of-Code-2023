dde0be33591e463cf139f58418e9f38c9cdb0543
Thomas Schmitt <t.e.schmitt@posteo.de>
2023-11-29 23:50:26 +0100

    adds .gitignore, utils, and run script

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2023
+
+## How to run
+
+To run day 3, part 2 do this:
+```shell
+$ ./run day03_part2 day03/input
+```
---
+++ utils/src/lib.rs
@@ -0,0 +1,29 @@
+use anyhow::{Context, Result};
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::{Path, PathBuf},
+};
+
+pub type Lines = io::Lines<BufReader<File>>;
+
+// Get contents of the file provided in args[1] as Iterator
+pub fn get_input_file() -> Result<Lines> {
+    read_lines(get_path_from_args()?)
+}
+
+fn read_lines<P>(filename: P) -> Result<Lines>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename).unwrap();
+    assert!(file.metadata()?.is_file(), "Provided path is not a file");
+    Ok(BufReader::new(file).lines())
+}
+
+fn get_path_from_args() -> Result<PathBuf> {
+    Ok(PathBuf::from(
+        env::args().nth(1).context("No argument passed")?,
+    ))
+}
