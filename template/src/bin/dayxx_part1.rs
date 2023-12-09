316baf064995b9cda8a470e8fb9c3294ee019274
Thomas Schmitt <t.e.schmitt@posteo.de>
2023-12-09 22:04:29 +0100

    small fixes

--- template/src/bin/dayxx_part1.rs
+++ template/src/bin/dayxx_part1.rs
@@ -4,5 +4,7 @@ use utils::get_input_file;
 fn main() -> Result<()> {
     let data = get_input_file()?;
 
+    // all_inputs(&data).map_err(|err| err.to_owned())?;
+
     Ok(())
 }
--- template/src/bin/dayxx_part2.rs
+++ template/src/bin/dayxx_part2.rs
@@ -3,18 +3,8 @@ use utils::get_input_file;
 
 fn main() -> Result<()> {
     let data = get_input_file()?;
-    let x: Vec<u8> = data
-        .filter_map(|s| {
-            if let Ok(s) = s {
-                if let Some((_, n)) = s.rsplit_once('r') {
-                    if let Ok(u) = n.parse::<u8>() {
-                        return Some(u);
-                    }
-                }
-            }
-            None
-        })
-        .collect();
-    println!("{:?}", x);
+
+    // all_inputs(&data).map_err(|err| err.to_owned())?;
+
     Ok(())
 }
