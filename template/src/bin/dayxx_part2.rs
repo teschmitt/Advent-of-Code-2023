e3561399877a0aa4d51791329ea92c5f27afae9f
Thomas Schmitt <t.e.schmitt@posteo.de>
2023-12-10 16:20:27 +0100

    small fixes

--- template/src/bin/dayxx_part2.rs
+++ template/src/bin/dayxx_part2.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use utils::get_input_file;
+use utils::get_input_file_as_string;
 
 fn main() -> Result<()> {
-    let data = get_input_file()?;
+    let data = get_input_file_as_string()?;
 
     // all_inputs(&data).map_err(|err| err.to_owned())?;
 
